5702524a0375e1b846c7b5f29728913c823a301c
Aleksandre Ghvineria <Ghvinerias@gmail.com>
2024-11-16 10:06:33 +0000

    start from step 1

--- Zabbix-Client/go.mod
+++ Zabbix-Client/go.mod
@@ -1,5 +1,3 @@
 module Zabbix-Client
 
 go 1.18
-
-require github.com/cavaliercoder/go-zabbix v0.0.0-20230131181515-93725c39d639
--- Zabbix-Client/main.go
+++ Zabbix-Client/main.go
@@ -6,12 +6,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-const (
-	apiURL = "https://zabbix.slick.ge/api_jsonrpc.php"
-	apiKey = "xxx"
-)
+var apiURL string
+var apiKey string
+
+func init() {
+	baseURL := os.Getenv("ZABBIX_SLICK_GE_API_URL")
+	apiKey = os.Getenv("ZABBIX_SLICK_GE_API_KEY")
+	if !strings.HasSuffix(baseURL, "/") {
+		baseURL += "/"
+	}
+	apiURL = baseURL + "api_jsonrpc.php"
+}
 
 type ZabbixRequest struct {
 	Jsonrpc string      `json:"jsonrpc"`
@@ -31,29 +40,26 @@ type ZabbixResponse struct {
 func main() {
 	hostname := "Web Monitoring"
 
-	// Step 1: Get Host ID using Hostname
+	//Get Host ID by Host Name, for later used in creating Items and Triggers
 	hostID, err := getHostID(hostname)
 	if err != nil {
 		log.Fatalf("Failed to get host ID: %v", err)
 	}
-
 	fmt.Printf("Host ID for '%s' is: %s\n", hostname, hostID)
 
-	// Step 2: Create an item using the retrieved hostID
-	itemID, err := createItem(apiKey, hostID, "slick.ge/api/example (HealthCheck)", "slick.ge_api/example_HealthCheck", "https://slick.ge/api/api/example/HealthCheck", "1m", 4)
-	if err != nil {
-		log.Fatalf("Failed to create item: %v", err)
-	}
+	// //Create an Item, using parameters Provided.
+	// itemID, err := createItem(apiKey, hostID, "slick.ge/api/example (HealthCheck)", "slick.ge_api/example_HealthCheck", "https://slick.ge/api/api/example/HealthCheck", "1m", 4)
+	// if err != nil {
+	// 	log.Fatalf("Failed to create item: %v", err)
+	// }
+	// fmt.Printf("Item created with ID: %s\n", itemID)
 
-	fmt.Printf("Item created with ID: %s\n", itemID)
-
-	// Step 3: Create a trigger using the created item key
-	triggerID, err := createTrigger(apiKey, "slick.ge/api/example (HealthCheck)", `last(/Web Monitoring/slick.ge_api/example_HealthCheck)<>"Healthy"`, "https://slick.ge/api/api/example/HealthCheck")
-	if err != nil {
-		log.Fatalf("Failed to create trigger: %v", err)
-	}
-
-	fmt.Printf("Trigger created with ID: %s\n", triggerID)
+	// //Create a trigger using the created item key
+	// triggerID, err := createTrigger(apiKey, "slick.ge/api/example (HealthCheck)", `last(/Web Monitoring/slick.ge_api/example_HealthCheck)<>"Healthy"`, "https://slick.ge/api/api/example/HealthCheck")
+	// if err != nil {
+	// 	log.Fatalf("Failed to create trigger: %v", err)
+	// }
+	// fmt.Printf("Trigger created with ID: %s\n", triggerID)
 }
 
 func getHostID(hostname string) (string, error) {
@@ -91,14 +97,14 @@ func getHostID(hostname string) (string, error) {
 
 func createItem(apiKey, hostID, name, key_, url, delay string, valueType int) (string, error) {
 	itemParams := map[string]interface{}{
-		"hostid":     hostID,
-		"name":       name,
-		"key_":       key_,
-		"type":       19, // HTTP Agent
-		"value_type": valueType,
-		"url":        url,
-		"delay":      delay,
-		"timeout":    "30",
+		"hostid":       hostID,
+		"name":         name,
+		"key_":         key_,
+		"type":         19, // HTTP Agent
+		"value_type":   valueType,
+		"url":          url,
+		"delay":        delay,
+		"timeout":      "30",
 		"status_codes": "",
 		"preprocessing": []map[string]interface{}{
 			{
@@ -160,7 +166,7 @@ func createTrigger(apiKey, description, expression, url string) (string, error) 
 	triggerParams := map[string]interface{}{
 		"description": description,
 		"expression":  expression,
-		"url":    url,
+		"url":         url,
 		"priority":    4,
 	}
 
